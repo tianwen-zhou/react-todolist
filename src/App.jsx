01a4250805645c3c6d168a6ee19e0a34598f2370
mark <zhoutianwen.999@gmail.com>
2024-11-22 23:49:13 +1300

    add remote request from axios and cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
-import React, { Component} from 'react';
+import React, { Component, useState, useEffect} from 'react';
 import './App.css';
 import Footer from './component/Footer'
 import Header from './component/Header'
 import List from './component/List'
+import { createTodo, updateTodo, deleteTodo, getTodos } from "./api/todoService";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+      super(props);
+      this.state = {
+        todos: [], // 保存 ToDo 列表
+      };
+    }
+
+    // // 加载 ToDo 列表
+    // fetchTodos = async () => {
+    //   try {
+    //     const todos = await getTodos();
+    //     this.setState({ todos });
+    //     console.log(todos)
+    //   } catch (error) {
+    //     console.error("Error fetching todos:", error);
+    //   }
+    // };
+
+    // 组件加载后调用 API 获取 ToDo 列表
+  async componentDidMount() {
+    try {
+      const todoObjs = await getTodos();
+      const todos = todoObjs.map( (todoObj)=> {
+        return  {id: todoObj.id, name:todoObj.title, done: todoObj.isCompleted}
+      })
+      this.setState({ todos });
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
   }
 
+  //     // 创建 ToDo
+  //  handleCreate = async () => {
+  //   const newTodo = { title, isCompleted };
+  //   try {
+  //     const createdTodo = await createTodo(newTodo);
+  //     setTodos([...todos, createdTodo]);
+  //     setTitle("");
+  //     setIsCompleted(false);
+  //   } catch (error) {
+  //     console.error("Error creating todo:", error);
+  //   }
+  // };
 
-  
-  state = {
-    todos: [
-      {id:'001', name:'coding', done: true},
-      {id:'002', name:'sleeping', done: true},
-      {id:'003', name:'reading', done: false},
-      {id:'004', name:'talking', done: false},
-    ],
-  };
+  // // 更新 ToDo
+  //  handleUpdate = async (id) => {
+  //   const updatedTodo = { title: "Updated Title", isCompleted: true };
+  //   try {
+  //     const result = await updateTodo(id, updatedTodo);
+  //     setTodos(todos.map((todo) => (todo.id === id ? result : todo)));
+  //   } catch (error) {
+  //     console.error("Error updating todo:", error);
+  //   }
+  // };
+
+  // // 删除 ToDo
+  //  handleDelete = async (id) => {
+  //   try {
+  //     await deleteTodo(id);
+  //     setTodos(todos.filter((todo) => todo.id !== id));
+  //   } catch (error) {
+  //     console.error("Error deleting todo:", error);
+  //   }
+  // };
+
+  // state = {
+  //   todos: [
+  //     {id:'001', name:'coding', done: true},
+  //     {id:'002', name:'sleeping', done: true},
+  //     {id:'003', name:'reading', done: false},
+  //     {id:'004', name:'talking', done: false},
+  //   ],
+  // };
 
    addTodo = (todoObj) =>{
      const {todos} = this.state
